2d15aa528bbcd40fff80b97c2b30fb9ba5d507b4
Stadrem <106669174+Stadrem@users.noreply.github.com>
2024-07-24 14:26:50 +0900

    tt

---
+++ Assets/Scripts/Dongsu/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once p르라라라라라라ㅏ라라ㅁㅁㅁ
+    void Update()
+    {
+        
+    }
+}
