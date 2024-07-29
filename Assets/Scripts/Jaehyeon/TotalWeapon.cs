2b933ce0f458e3a5b9e7495b2d91b202c2754b04
jaehyeond <pjhyun9711@gmail.com>
2024-07-29 13:12:54 +0900

    UI상호작용

--- Assets/Scripts/Jaehyeon/TotalWeapon.cs
+++ Assets/Scripts/Jaehyeon/TotalWeapon.cs
@@ -14,14 +14,13 @@ public class TotalWeapon : MonoBehaviour
     public bool AutoFire = false; // 자동 발사 여부 (기관총 등)
     public float fireRate = 0.1f; // 발사 간격 (자동 발사용)
     public int magazineSize = 30; // 탄창 크기
-    public int currentBullet; // 현재 탄약 수
     public bool needMag = true; // 탄창이 필요한지 여부
 
     private float lastFireTime = 0f;
 
     void Start()
     {
-        currentBullet = magazineSize;
+        GameUIManager.instance.bulletCurrent = magazineSize;
     }
 
     void Update()
@@ -35,11 +34,11 @@ public class TotalWeapon : MonoBehaviour
         // 만약 자동 발사 모드가 on 이라면
         if (AutoFire)
         {
-            // 마우스 좌클릭이 눌린 상태, 이전 발사 이후로 fireRate만큼의 시간이 지skaus
+            // 마우스 좌클릭이 눌린 상태, 이전 발사 이후로 fireRate만큼의 시간이 지남
             if (Input.GetButton("Fire1") && Time.time - lastFireTime >= fireRate)
             {
                 // 탄창이 필요하지 않거나, 현재 탄약이 0보다 크다면 발사
-                if (!needMag || currentBullet > 0)
+                if (!needMag || GameUIManager.instance.BulletShoot())
                 {
                     Shoot();
                     // 마지막 발사 시간을 현재 시간으로 갱신
@@ -55,10 +54,8 @@ public class TotalWeapon : MonoBehaviour
         {
             if (Input.GetButtonDown("Fire1"))
             {
-                
-                if (!needMag || currentBullet > 0)
+                if (!needMag || GameUIManager.instance.BulletShoot())
                 {
-                    Debug.Log(2);
                     Shoot();
                 }
                 else
@@ -75,12 +72,6 @@ public class TotalWeapon : MonoBehaviour
         {
             Debug.LogError("Inspector창에 무기상태가 안들어감");
             return;
-        }
-
-        // 총기류의 경우 탄약 감소
-        if (needMag)
-        {
-            currentBullet--;
         }
 
         // Raycast를 이용해 적 감지 및 데미지 적용
@@ -113,8 +104,8 @@ public class TotalWeapon : MonoBehaviour
         if (needMag)
         {
             Debug.Log("장전중...");
-            currentBullet = magazineSize;
-            Debug.Log("장전완료. 현재 총알: " + currentBullet);
+            GameUIManager.instance.Reload(magazineSize);
+            Debug.Log("장전완료. 현재 총알: " + GameUIManager.instance.bulletCurrent);
         }
     }
 }
