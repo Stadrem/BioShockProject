1afefd5e3ce2e8c46f7fe1815906077a89fdfbde
jaehyeond <pjhyun9711@gmail.com>
2024-08-14 16:28:40 +0900

    폭탄 던지기 구현

--- Assets/Scripts/Jaehyeon/ObjectBomb.cs
+++ Assets/Scripts/Jaehyeon/ObjectBomb.cs
@@ -1,23 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ObjectBomb : MonoBehaviour
 {
+
+    public GameObject effectPrefab;
+    public GameObject fireEffect;
+    public LayerMask layerMask;
+    public LayerMask throwdamMask;
+    bool trigger = false;
+    public float range = 10f;
+
+    
     void OnCollisionEnter(Collision other)
     {
-        Damaged dam = other.gameObject.GetComponent<Damaged>();
-        if (dam != null)
+        if(trigger == false)
         {
-            dam.Damage(5, "type");
-        }
-        Destroy(this); // 충돌 후 컴포넌트를 제거하여 계속해서 데미지를 입히지 않도록 함
 
-        BossDamaged Bodam = other.gameObject.GetComponent<BossDamaged>();
-        if(Bodam != null)
-        {
-            Bodam.Damaged(5, "type");
+            GameObject bulletImpact = Instantiate(effectPrefab);
+
+            Collider[] hitColliders = Physics.OverlapSphere(bulletImpact.transform.position, range);
+
+            foreach (Collider collider in hitColliders)
+            {
+                // Enemy인지 확인하고 Damage 함수를 호출
+                Damaged dam = collider.gameObject.GetComponent<Damaged>();
+                if (dam != null)
+                {
+                    dam.Damage(5, "type");
+                }
+
+                // Boss인지 확인하고 Damaged 함수를 호출
+                BossDamaged Bodam = collider.gameObject.GetComponent<BossDamaged>();
+                if (Bodam != null)
+                {
+                    Bodam.Damaged(5, "type");
+                }
+            }
+
+            // bulletImpact 위치 초기화
+            bulletImpact.transform.position = transform.position;
+
+            trigger = true;
+
+            Destroy(gameObject, 0.25f);
         }
-        Destroy(this); // 충돌 후 컴포넌트를 제거하여 계속해서 데미지를 입히지 않도록 함
+        
     }
 }
